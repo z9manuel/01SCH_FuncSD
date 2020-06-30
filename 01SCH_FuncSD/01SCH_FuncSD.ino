1b38044e27eeea58c45cb88d139c8b1198a67a1b
José Manuel Rodríguez Morales <z9manuel@yahoo.com.mx>
2020-06-29 22:56:07 -0500

    Funciones MicroSD terminadas y optimizadas para disositivos en carniceria.

--- 01SCH_FuncSD/01SCH_FuncSD.ino
+++ 01SCH_FuncSD/01SCH_FuncSD.ino
@@ -21,7 +21,7 @@ void setup() {
 		delay(10);
 	}
 	SD_leerLog();
-	SD_borrarLog() ? Serial.println("Log borrado.") : Serial.println("Log no borrado");
+	//SD_borrarLog() ? Serial.println("Log borrado.") : Serial.println("Log no borrado");
 }
 
 
@@ -30,83 +30,73 @@ void loop() {
 
 
 bool SD_leerLog() {
-	// Me quede en esta función 
-	SD.begin(SD_CS);
-	if (!SD.begin(SD_CS)) {
-		Serial.println("Error modulo SD!");
-		return 0;
-	}
-	uint8_t cardType = SD.cardType();
-	if (cardType == CARD_NONE) {
-		Serial.println("Error tarjeta SD!");
-		return 0;
-	}
-	if (!SD.begin(SD_CS)) {
-		Serial.println("ERROR - Falla en tarjeta SD!");
-		return 0;
-	}
-	File dataLog = SD.open("/log.txt", FILE_READ);
-	if (dataLog) {
-		int renglon = 0;
-		String confLine;
-		dataLog.position();
-		while (dataLog.available() && renglon<51) {
-			confLine = dataLog.readStringUntil('\n');
-			Serial.println(confLine);
-			renglon=renglon+1;
+	if (SD_validar()) {
+		File dataLog = SD.open("/log.txt", FILE_READ);
+		if (dataLog) {
+			bool enviado = 0;
+			String linea;
+			dataLog.position();
+			while (dataLog.available()) {
+				linea = dataLog.readStringUntil('\n');
+				enviado = enviar_a_API(linea);
+				if (enviado == false) {
+					return false;
+				}
+			}
+			dataLog.close();
+			if (SD_borrarLog()) {
+				return 1;
+			}
 		}
-		Serial.println(String(renglon) + " renglones.");
-		while (dataLog.available()) {
-			confLine = dataLog.readString();
-			Serial.println(confLine);
-		}
-		dataLog.close();
-		return 1;
 	}
+	return false;
 }
 
 bool SD_escribirLog(String cadena) {
-	SD.begin(SD_CS);
-	if (!SD.begin(SD_CS)) {
-		Serial.println("Error modulo SD!");
-		return 0;
+	if (SD_validar()) {
+		SD.begin(SD_CS);
+		File dataLog = SD.open("/log.txt", FILE_APPEND);
+		if (dataLog) {
+			dataLog.println(cadena);
+			dataLog.close();
+			Serial.println(cadena);
+			return 1;
+		}
 	}
-	uint8_t cardType = SD.cardType();
-	if (cardType == CARD_NONE) {
-		Serial.println("Error tarjeta SD!");
-		return 0;
-	}
-	if (!SD.begin(SD_CS)) {
-		Serial.println("ERROR - Falla en tarjeta SD!");
-		return 0;
-	}
-	File dataLog = SD.open("/log.txt", FILE_APPEND);
-	if (dataLog) {
-		dataLog.println(cadena);
-		dataLog.close();
-		Serial.println(cadena);
-		return 1;
-	}
+	return false;
 }
 
 bool SD_borrarLog() {
+	if (SD_validar()) {
+		SD.begin(SD_CS);
+		if (SD.remove("/log.txt")) {
+			Serial.println("Registro borrado.");
+			schFile.close();
+			return 1;
+		}
+	}
+	return false;
+}
+
+bool SD_validar() {
 	SD.begin(SD_CS);
 	if (!SD.begin(SD_CS)) {
 		Serial.println("Error modulo SD!");
-		return 0;
+		return false;
 	}
 	uint8_t cardType = SD.cardType();
 	if (cardType == CARD_NONE) {
 		Serial.println("Error tarjeta SD!");
-		return 0;
+		return false;
 	}
 	if (!SD.begin(SD_CS)) {
 		Serial.println("ERROR - Falla en tarjeta SD!");
-		return 0;
+		return false;
 	}
-	if (SD.remove("/log.txt")) {
-		Serial.println("Registro borrado.");
-		schFile.close();
-		return 1;
-	}
+	return 1;
+}
+
+bool  enviar_a_API(String dato) {
+	Serial.println (dato);
+	return 1;
 }
